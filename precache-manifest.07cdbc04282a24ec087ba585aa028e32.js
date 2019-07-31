4e99d13cfd9d40ccfc1c394bbe180433ab764e74
Пользователь <Sviatoslv>
2019-07-31 16:21:33 +0300

    Updates

---
+++ precache-manifest.07cdbc04282a24ec087ba585aa028e32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397ce0b22519404dd8ba390ade5f6e25",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "576080689e04f31ca9fa",
+    "url": "/react_tabs-with-router/static/css/main.9971a7b5.chunk.css"
+  },
+  {
+    "revision": "515b29a72bc7e88a27cb",
+    "url": "/react_tabs-with-router/static/js/2.af74f105.chunk.js"
+  },
+  {
+    "revision": "576080689e04f31ca9fa",
+    "url": "/react_tabs-with-router/static/js/main.e97346d5.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
